4f89ac803222eeaef439ec716731e87dda89eab7
Alexey <Global@mail.ru>
2016-10-26 20:19:24 +0300

    Start page of site and nb to gitignore list

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+	<h1>Ho-ho-ho</h1>
+	<img src="http://az616578.vo.msecnd.net/files/2015/12/20/635861789284310957-171964277_1sa.jpg" style="width: 100%;"
+    </body>
+</html>
